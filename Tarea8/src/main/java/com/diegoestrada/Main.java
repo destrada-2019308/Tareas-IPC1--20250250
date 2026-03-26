82bb38b6cf874ac3279f4f3b7bf7f59d47d7482d
destrada-2019308 <destrada-2019308@kinal.edu.gt>
2026-03-25 23:42:11 -0600

    Add Tarea 8

---
+++ Tarea8/src/main/java/com/diegoestrada/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.diegoestrada;
+
+import com.diegoestrada.view.MainView;
+
+/**
+ *
+ * @author estra
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        MainView mainView = new MainView();
+        mainView.setVisible(true);
+    }
+}
